593b7ef82aeec03ae95affb5307f24a606e45e29
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-17 08:28:13 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -14,7 +14,7 @@ var config = {
             image: '',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheap. As a typical East Coast large city, the urban core is dense, so there is a lot within reach of a 15 minute ride... even mountain bike trails. Paired with the public transit infrastructure, cycling can be more efficient and much less expensive than driving (and parking) a car.',
             location: {
-                center: [-62.389404, -5.154276],
+                center: [-77.603475, -11.216189],
                 zoom: 4.5,
                 bearing: -20,
                 pitch: 41.5
