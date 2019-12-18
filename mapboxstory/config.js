4a9aaea67125316fdd90db5706bf9b64a81feb45
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-18 21:32:37 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -38,7 +38,7 @@ var config = {
             image: '',
             description: 'Philaun, the public transit infrastructun driving (and parking)and cheat and yarn and bilbo and frodo and golum the vapour boy and dogs and Finn and Allan and Anya',
             location: {
-                center: [19.552837, -3.210479],
+                center: [102.996634, 11.834921],
                 bearing: 0,
                 zoom: 4.5,
                 pitch: 30.5
