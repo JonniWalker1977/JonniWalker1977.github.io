b06664fb1420c92088df5b6bac87c9521ac9c6dd
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-19 08:36:26 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -10,7 +10,7 @@ var config = {
         {
             id: 'THEAMAZON',
             title: 'THE AMAZON', 
-            image: '',
+            image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2018/01/shutterstock_118980505.jpg',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses with long maines and furry badgers who dont get hunted with foxes and hares against rabbits and cats',
                 location: {
                 center: [-77.603475, -11.216189],
