9249492367b427682049efae0a26cbc06f86560f
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-21 14:38:59 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -10,7 +10,7 @@ var config = {
         {
             id: 'THEAMAZON',
             title: 'WHAT WOULD A MAP OF THE EARTH LOOK LIKE IF ALL THE RAINFORESTS WERE DEFORESTED?',
-            description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two.  </br> </p> </br> </p> LEGEND </p> </p>',
+            description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> </p> LEGEND </p> </p>',
              image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
     
             location: {
