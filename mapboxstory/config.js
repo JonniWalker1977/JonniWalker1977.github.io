a3f1df222e5b20d216d1831ff05496b732704f52
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-27 10:04:47 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -58,7 +58,7 @@ var config = {
         {
             id: 'IndiaSriLanka',
             title: 'INDIA & SRI LANKA',
-            image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Globe-sat.png',
+            image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Globe-sat-1.png',
             description: 'Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents. </br> </p> </br> </p> LOCATION MAP </p> </p>',
             location: {
              center: [76.415286, 16.999026],
