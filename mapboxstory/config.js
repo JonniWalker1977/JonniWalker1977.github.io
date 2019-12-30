72344f0a8e2c8d755aaeb0e93fe8b8bc15fcf3bb
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-30 15:09:17 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -25,19 +25,19 @@ var config = {
             onChapterEnter: [
                 {
                     layer: 'archive-6-9g1e5q',
-                    opacity: 0.55
+                    opacity: 0
                 }
             ],
             onChapterExit: [
                 {
                     layer: 'archive-6-9g1e5q',
-                    opacity: 0.55
+                    opacity: 0
                 }
                 ]
         },
          {
              id: 'THEAMAZON',
-            title: 'WHAT WOULD A MAP OF THE EARTH LOOK LIKE IF ALL THE RAINFORESTS WERE DEFORESTED?',
+            title: 'WHAT IF THE AMAZON WAS DEFORESTED IN A DAY?',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> </p> LOCATION MAP </p> </p>',
              image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
     
