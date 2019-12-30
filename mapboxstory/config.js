cede44f5ef033c71bc311af747ac950da9ca7313
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2019-12-30 13:38:29 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -21,7 +21,7 @@ var config = {
                 pitch: 41.5
             },
            
-            id: 'THEAMAZON',
+            id: 'THEAMAZON2',
             title: 'GONE?',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> </p> LOCATION MAP </p> </p>',
              image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
