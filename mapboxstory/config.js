8e045416829c898743c0b30db71c6d90d46766fb
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-03 09:33:06 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -59,7 +59,7 @@ var config = {
             onChapterEnter: [
                 {
                     layer: 'archive-6-9g1e5q',
-                     "fill-opacity-transition": {
+                     "fill-opacity-transition": { duration: 10000
        
       },
                     opacity: 0.55
