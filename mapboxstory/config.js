8f6fd628afc48593a7bc0c646a8d2fced9b8e9e3
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-06 09:48:44 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -29,7 +29,20 @@ var config = {
                     opacity: 0
                 },
           
-           
+              { duration: 10000,
+               delay: 0
+              
+               
+ 
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'archive-6-9g1e5q',
+                    
+                                                  "fill-opacity-transition": {
+        duration: 0.1
+      },opacity: 0.55
                     
     
                 }
