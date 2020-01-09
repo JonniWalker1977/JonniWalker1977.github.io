415064a7b6a69cadb77a40105aeaf7d1f304c0c7
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-09 10:46:43 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -11,7 +11,7 @@ var config = {
         {
              id: 'THEAMAZON',
             title: 'WHAT WOULD THE EARTH LOOK LIKE IF ALL THE SUB-TROPICAL RAINFORESTS WERE DEFORESTED?',
-            description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> </p> SCROLL DOWN </p> WITH EACH SCROLL YOU ARE TAKING AN ACTION</p>',
+            description: 'Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> <p><strong> SCROLL DOWN <p><strong> WITH EACH SCROLL YOU ARE TAKING AN ACTION</p>',
              image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Scroll-White-1.gif', 
             image2: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
     
