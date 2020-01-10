a8bc48c9c226a8ad3f04b74b6a299b602cc400cd
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-10 11:33:42 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -63,7 +63,7 @@ var config = {
                 {
                     layer: 'archive-6-9g1e5q',
                     
-        opacity: 0.55,
+        opacity: 0.65,
                     duration: 2000
       
                     
