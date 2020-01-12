edcbcd14b2d77e054c5eec2da66f081e80e8f40d
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-12 19:02:04 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -4,8 +4,6 @@ var config = {
     showMarkers: false,
     theme: 'dark',
     alignment: 'left',
-    title: 'DYSTOPIAN DEFORESTATION',
-    byline: '',
     footer: '',
     animation: 'fadein',
     chapters: [
