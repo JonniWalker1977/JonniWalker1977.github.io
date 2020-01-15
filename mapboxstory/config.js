f8397d2bbefeef2f075f13ecf5be70af64e6e183
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-15 08:49:19 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -10,9 +10,9 @@ var config = {
         {
              id: 'THEAMAZON',
             title: 'DYSTOPIAN DEFORESTION:',
-            description: 'What would a map of the Earth look like if all the sub-tropical rainforests were deforested in a day? <br/> </p> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? <br/> </p> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? </br> </p> </br> <p><strong> SCROLL DOWN <img src="https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Scroll-White-1.gif"> <p><strong> With each scroll YOU are taking an ACTION</p>',
-            image2: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
-            image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Scroll-White-1.gif',
+            description: 'What would a map of the Earth look like if all the sub-tropical rainforests were deforested in a day? <br/> </p> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? <br/> </p> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? </br> </p> </br> <p><strong> SCROLL DOWN </br> </p> <img src="https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Scroll-White-1.gif"> <p><strong> With each scroll YOU are taking an ACTION</p>',
+            image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
+            
     
             location: {
                 center: [-73.603475, -11.216189],
