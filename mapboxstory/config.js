aca2abb34b14430b4ae1398f1ab1782f82c5e3f9
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-17 10:19:07 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -9,7 +9,7 @@ var config = {
     chapters: [
         {
              id: 'THEAMAZON',
-            title: ' A POSSIBLE FUTURE: DYSTOPIAN DEFORESTATION',
+            title: ' A Possible future?: Dystopian Deforestaion',
             description: ' ___________________________________ What would a map of the Earth look like if all the sub-tropical rainforests were deforested in a day? <br/> </p> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? <br/> </p> Making up one thirf of around Philadelphia on two horse around the field. This not going to work is it? Footbal pitch idea? </br> ________________________  <p> SCROLL DOWN </br> <img src="https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Scroll-White-1.gif"> </br> With each scroll YOU are taking an ACTION</br> Those ACTIONS ARE important.',
             image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-19-at-13.png', 
             
