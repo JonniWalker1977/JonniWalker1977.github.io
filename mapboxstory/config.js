382c56d67f5e2362f8949a6db17d795280adab30
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-17 16:07:36 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -45,7 +45,7 @@ var config = {
         },
          {
              id: 'THEAMAZONS',
-            title: 'What if the Amazon was Deforested ina Day?',
+            title: 'What if the Amazon was Deforested in a Day?',
             description: '___________________________________ Getting around Philadelphia on two wheels is fast, fun, and cheaters and cats and dogs and rabbits and horses and Kakapo and Takahe and rats<br/> </p> </p> Fast, fun, and cheaters and cats its and catsGetting around Philadelphia on two horse around the field.  </br> </p> </br> </p> LOCATION MAP </p> </p>',
              image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2020/01/Screen-Shot-2020-01-11-at-18-2.png', 
             
