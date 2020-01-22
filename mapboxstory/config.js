340ae50d414191d9dd5750b3c7cd33816b4dbf42
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-22 10:42:10 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -137,7 +137,8 @@ var config = {
             id: 'Indo',
             title: 'Indomalayan Rainforest Realm',
             image: 'https://www.jonniwalkerdatablog.com/wp-content/uploads/2019/12/Globe-sat-1.png',
-            description: '___________________________________ Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents. </br> </p> </br> </p> LOCATION MAP </p> </p>',
+            description: '___________________________________ Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, The system initally was focused on Center City, The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents. </br> </p> </br> </p> LOCATION MAP </p> </p>',
+
             location: {
              center: [96.801867, 4.318879],
              duration: 4500,
