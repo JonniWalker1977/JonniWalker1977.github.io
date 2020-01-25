256b31e9f0d6f6886f3e0a5e6d6cde5899fc000c
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-01-25 16:03:38 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -194,7 +194,7 @@ var config = {
             image: '',
             description: '________________________________ <br/> A short ride along the Schuylkill River Trail from the Art Museum, Belmont is a twisty, log-ridden rollercoaster of a trail network. It is easy to get turned around, the underbrush is at times impenetrable, and short steep sections come out of nowhere. In other words, it\'s really fun',
             location: {
-                center: [112.042529, 3.378059],
+                center: [115.042529, 3.378059],
                 duration: 4500,
                 zoom: 3.7,
                 pitch: 0,
