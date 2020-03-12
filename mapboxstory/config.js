5bad95f1726a5da1eb5c897096e138913c83d2d1
JonniWalker1977 <58425019+JonniWalker1977@users.noreply.github.com>
2020-03-12 13:47:25 +0000

    commit

--- mapboxstory/config.js
+++ mapboxstory/config.js
@@ -239,7 +239,7 @@ var config = {
             location: {
                 center: [114.162529, 3.378059],
                 duration: 4500,
-                zoom: 4,
+                zoom: 3.9,
                 pitch: 0,
                 bearing: 0
             },
